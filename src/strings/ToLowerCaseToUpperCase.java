2d9672818870770f6e83b1c445fd54648daf7c28
Ahmet Duman <Ahmet.Duman@s.pioneercss.org>
2019-09-04 11:58:48 -0400

    df

--- src/strings/ToLowerCaseToUpperCase.java
+++ src/strings/ToLowerCaseToUpperCase.java
@@ -1,9 +1,11 @@
 package strings;
 
-public class ToLowerCaseToUpperCase {
+public class ToLowerCaseToUpperCase{
 
 	public static void main(String[] args) {
-		
+	String s= "Welcome to Java";
+	System.out.println(s.toLowerCase());
+		System.out.println(s.toUpperCase());
 	}
 
 }
